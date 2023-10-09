b7c98f91006cf5965bd8a6f30cb74e179858da38
Valentin Labrune <valentin.labrune@gmail.com>
2023-10-09 14:40:31 +0200

    Ex 4: create HomePage component

--- vue-oauth-microsoft-graph/src/App.vue
+++ vue-oauth-microsoft-graph/src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import HomePage from './pages/HomePage.vue'
 </script>
 
 <template>
@@ -9,6 +10,7 @@
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <HomePage />
   </div>
 </template>
 
---
+++ vue-oauth-microsoft-graph/src/pages/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        this is the HomePage page component
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    data() {
+        return {
+            // Your data goes here
+        }
+    },
+    methods: {
+        // Your methods go here
+    },
+    mounted() {
+        // Your mounted code goes here
+    }
+}
+</script>
+
+<style>
+/* Your styles go here */
+</style>
